4d8500f7b4583f2ae62574f535a7c894d351014e
Victor Zagorski <victor@zagorski.ru>
2013-07-03 13:39:27 +0800

    Added specs for serializer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,12 +22,12 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -56,14 +56,8 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with :truncation
   end
 
-  config.before :all, uses_fixtures: true do
-    @uses_fixtures ||= []
-    @uses_fixtures.push true
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.after :all, uses_fixtures: true do
-    DatabaseCleaner.clean
+  config.before :each do
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before :each, :js => true  do

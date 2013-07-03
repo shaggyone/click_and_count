bb9447d15c80ab96b17dda10ebc9eef10700e2b4
Victor Zagorski <victor@zagorski.ru>
2013-07-03 13:42:40 +0800

    Added specs for model

---
+++ spec/models/line_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Line do
+  it { should allow_mass_assignment_of :data }
+  it { should_not allow_mass_assignment_of :id }
+  it { should_not allow_mass_assignment_of :created_at }
+  it { should_not allow_mass_assignment_of :updated_at }
+end
